fde5c133297754607837dd41b0c3f7cdc075f527
Gleb Dolgich <gdolgich@riskcare.com>
2014-07-25 15:32:57 +0100

    Add test project and one test

---
+++ HelloWorldRxUITests/AppViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using HelloWorldRxUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldRxUITests
+{
+    [TestClass]
+    public class AppViewModelTests
+    {
+        [TestMethod]
+        public void TestGreeting()
+        {
+            var viewModel = new AppViewModel();
+            viewModel.Name = "Foo";
+            Assert.IsTrue(viewModel.Greeting.Equals("Hello Foo"));
+        }
+    }
+}
