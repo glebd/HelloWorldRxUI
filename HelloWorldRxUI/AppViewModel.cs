73ec30a7d1956158176c1b1b972c73c0634da49d
Gleb Dolgich <gdolgich@riskcare.com>
2014-07-25 14:08:48 +0100

    Add ViewModel, bindings, handle changes of Name

---
+++ HelloWorldRxUI/AppViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ReactiveUI;
+
+namespace HelloWorldRxUI
+{
+    public class AppViewModel : ReactiveObject
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { this.RaiseAndSetIfChanged(ref _name, value); }
+        }
+
+        private string _greeting;
+        public string Greeting
+        {
+            get { return _greeting; }
+            set { this.RaiseAndSetIfChanged(ref _greeting, value); }
+        }
+
+        public AppViewModel()
+        {
+            this.ObservableForProperty(x => x.Name, false, false)
+                .Subscribe(x =>
+                {
+                    Greeting = "Hello " + (string.IsNullOrEmpty(x.Value) ? "stranger" : x.Value);
+                });
+        }
+    }
+}
