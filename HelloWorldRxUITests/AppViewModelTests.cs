ec1b628d15d7cf654b18022b131af6eb8ce38d8c
Gleb Dolgich <gdolgich@riskcare.com>
2014-07-25 15:42:06 +0100

    Add a command test

--- HelloWorldRxUITests/AppViewModelTests.cs
+++ HelloWorldRxUITests/AppViewModelTests.cs
@@ -1,4 +1,5 @@
-﻿using HelloWorldRxUI;
+﻿using System;
+using HelloWorldRxUI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HelloWorldRxUITests
@@ -12,6 +13,16 @@ namespace HelloWorldRxUITests
             var viewModel = new AppViewModel();
             viewModel.Name = "Foo";
             Assert.IsTrue(viewModel.Greeting.Equals("Hello Foo"), "Greeting is {0} but should be 'Hello Foo'", viewModel.Greeting);
+        }
+
+        [TestMethod]
+        public void TestCommand()
+        {
+            var viewModel = new AppViewModel();
+            bool called = false;
+            viewModel.ContinueCommand.Subscribe(_ => called = true);
+            viewModel.ContinueCommand.Execute(null);
+            Assert.IsTrue(called, "Command action not called");
         }
     }
 }
